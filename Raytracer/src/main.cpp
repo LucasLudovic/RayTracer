a9c5c06064ca1dda7e19fa9e13bfebf6ce7d0ece
Valiance <lucas.ludovic@epitech.eu>
2025-04-30 15:05:47 +0200

    chore: add basic files

---
+++ Raytracer/src/main.cpp
@@ -0,0 +1,13 @@
+//
+// EPITECH PROJECT, 2025
+// Main
+// File description:
+// Main
+//
+
+#include "my_macros.hpp"
+
+int main(int argc, char **argv)
+{
+    return EPITECH_SUCCESS;
+}
---
+++ Raytracer/src/my_macros.hpp
@@ -0,0 +1,14 @@
+//
+// EPITECH PROJECT, 2025
+// My_macros
+// File description:
+// My_macros
+//
+
+#pragma once
+
+#define SUCCESS 0
+#define FAILURE -1
+
+#define EPITECH_SUCCESS 0
+#define EPITECH_FAILURE 84

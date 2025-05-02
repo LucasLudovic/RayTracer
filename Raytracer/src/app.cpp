9935ae9abd0117675b2130ce9f202aa4df911bfb
Valiance <lucas.ludovic@epitech.eu>
2025-05-02 17:23:34 +0200

    chore: cmakelists compile with core src

--- Raytracer/src/app.cpp
+++ Raytracer/src/app.cpp
@@ -6,9 +6,23 @@
 //
 
 #include "my_macros.hpp"
+#include "src/Raytracer/Raytracer.hpp"
+#include <iostream>
 #include <string>
+
+using raytracer::Raytracer;
 
 int execute_raytracer(const std::string &sceneFile)
 {
+    try {
+        raytracer::Raytracer raytracerCore(sceneFile);
+
+        raytracerCore.run();
+    } catch (const Raytracer::RaytracerError &error) {
+        std::cerr << "Application Error: " << error.what() << std::endl;
+        return EPITECH_FAILURE;
+    } catch (...) {
+        std::cerr << "Unknown error" << std::endl;
+    }
     return EPITECH_SUCCESS;
 }

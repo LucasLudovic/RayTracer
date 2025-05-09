aec85e2c24ac4e67000158099788298ebf5c9c0e
Yuuko <tilio.huart@epitech.eu>
2025-05-09 11:36:16 +0200

    feat: add lights object

---
+++ Primitives/Lights/src/Lights.hpp
@@ -0,0 +1,22 @@
+//
+// EPITECH PROJECT, 2025
+// Raytracer
+// File description:
+// Lights.hpp
+//
+
+#pragma once
+
+#include "AObject.hpp"
+
+namespace objects {
+    class Lights : public AObject {
+       public:
+        Lights();
+
+        class LightsError : public AObjectError {};
+
+
+       private:
+    };
+}  // namespace objects

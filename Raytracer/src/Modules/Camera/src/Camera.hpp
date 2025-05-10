a1c798bdf4561b1749906eb645911bf4b245391d
Yuuko <tilio.huart@epitech.eu>
2025-05-10 12:21:01 +0200

    feat: create basic classes for camera and lights modules

---
+++ Raytracer/src/Modules/Camera/src/Camera.hpp
@@ -0,0 +1,31 @@
+//
+// EPITECH PROJECT, 2025
+// Raytracer
+// File description:
+// Camera.hpp
+//
+
+#pragma once
+
+#include "Vector.hpp"
+
+namespace objects {
+    class Camera {
+       public:
+        Camera();
+
+        void setResolution(raytracer::Vector2<int> res);
+        raytracer::Vector2<int> getResolution() const;
+
+        void setRotation(raytracer::Vector3<int> rotation);
+        raytracer::Vector3<int> getRotation() const;
+
+        void setFieldOfVue(int value);
+        int getFieldOfView() const;
+
+       private:
+        raytracer::Vector2<int> _res;
+        raytracer::Vector3<int> _rotation;
+        int _fieldOfVue = 0;
+    };
+}  // namespace objects
---
+++ Raytracer/src/Modules/Lights/src/base.cpp
@@ -0,0 +1,10 @@
+//
+// EPITECH PROJECT, 2025
+// Base
+// File description:
+// Base functions
+//
+
+#include "Lights.hpp"
+
+objects::Lights::Lights() {}

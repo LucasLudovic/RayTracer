6e4a4096f9b918081f02312c8e636be3ebfe2391
Valiance <lucas.ludovic@epitech.eu>
2025-05-12 10:57:23 +0200

    feat: add ray class to all objects

--- Raytracer/src/Raycast/Raycast.hpp
+++ Raytracer/src/Raycast/Raycast.hpp
@@ -10,17 +10,21 @@
 #include "Vector.hpp"
 
 namespace raytracer {
-    class raycast {
+    class Raycast {
        public:
-        raycast() {};
-        raycast(
+        Raycast() {};
+        Raycast(
             const Vector3<double> &origin, const Vector3<double> &direction)
             : _origin(origin), _direction(direction) {};
-        ~raycast() = default;
+        ~Raycast() = default;
 
         Vector3<double> getOrigin() { return this->_origin; };
 
         Vector3<double> getDirection() { return this->_direction; };
+        Vector3<double> at(double length) const
+        {
+            return this->_origin + (this->_direction * length);
+        }
 
        private:
         Vector3<double> _origin;

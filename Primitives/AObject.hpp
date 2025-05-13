7b2de07e05f65bd1594289943e860db15cd41f67
Pomalone <o.c.loulou@gmail.com>
2025-05-13 12:23:57 +0200

    feat: add materials setters and getters for Objects

--- Primitives/AObject.hpp
+++ Primitives/AObject.hpp
@@ -9,56 +9,87 @@
 
 #include "../Utils/Vector.hpp"
 #include "IObject.hpp"
+#include "Material.hpp"
 
 namespace objects {
 
-    class AObject : public objects::IObject {
+class AObject : public objects::IObject
+{
+   public:
+    AObject() {};
+
+    class AObjectError : public IObjectError
+    {
        public:
-        AObject() {};
+        AObjectError(const std::string &msg) : _msg(msg) {};
 
-        class AObjectError : public IObjectError {
-           public:
-            AObjectError(const std::string &msg) : _msg(msg) {};
-
-            [[nodiscard]] const char *what() const noexcept override
-            {
-                return this->_msg.c_str();
-            };
-
-           private:
-            std::string _msg;
+        [[nodiscard]] const char *what() const noexcept override
+        {
+            return this->_msg.c_str();
         };
 
-        std::string getType() const override = 0;
-
-        std::unique_ptr<IObject> clone() const override = 0;
-
-        void setPosition(raytracer::Vector3<int> pos) override
-        {
-            this->_position = pos;
-        }
-
-        raytracer::Vector3<int> getPosition() const override
-        {
-            return this->_position;
-        }
-
-        void setColor(raytracer::Vector3<int> rgb) override
-        {
-            this->_color = rgb;
-        }
-
-        raytracer::Vector3<int> getColor() const override
-        {
-            return this->_color;
-        }
-
-        metaData_t &getMetaData() override { return this->_metaData; };
-
-       protected:
-        std::string _type;
-        raytracer::Vector3<int> _position;
-        raytracer::Vector3<int> _color;
-        metaData_t _metaData;
+       private:
+        std::string _msg;
     };
+
+    std::string getType() const override = 0;
+
+    std::unique_ptr<IObject> clone() const override = 0;
+
+    void setPosition(raytracer::Vector3<int> pos) override
+    {
+        this->_position = pos;
+    }
+
+    raytracer::Vector3<int> getPosition() const override
+    {
+        return this->_position;
+    }
+
+    void setColor(raytracer::Vector3<double> rgb) override
+    {
+        this->_material.set_color(rgb);
+    }
+
+    void setColor(double r, double g, double b) override
+    {
+        this->_material.set_color(r, g, b);
+    }
+
+    raytracer::Vector3<double> getColor() const override
+    {
+        return this->_material.get_color();
+    }
+
+    void setTransparence(double value) override
+    {
+        this->_material.set_transparence(value);
+    }
+
+    double getTransparence() const override
+    {
+        return this->_material.get_transparence();
+    }
+
+    void setReflection(double value) override
+    {
+        this->_material.set_reflection(value);
+    }
+
+    double getReflection() const override
+    {
+        return this->_material.get_reflection();
+    }
+
+    metaData_t &getMetaData() override
+    {
+        return this->_metaData;
+    };
+
+   protected:
+    std::string _type;
+    raytracer::Vector3<int> _position;
+    objects::Material _material;
+    metaData_t _metaData;
+};
 }  // namespace objects
--- Primitives/IObject.hpp
+++ Primitives/IObject.hpp
@@ -7,42 +7,51 @@
 
 #pragma once
 
-#include "../Utils/Vector.hpp"
 #include <exception>
 #include <memory>
 #include <optional>
 #include <string>
 
+#include "../Utils/Vector.hpp"
+
 namespace raytracer {
-    class Raycast;
+class Raycast;
 }
 
 namespace objects {
 
-    typedef struct metaData {
-        std::optional<int> radius;
-    } metaData_t;
+typedef struct metaData
+{
+    std::optional<int> radius;
+} metaData_t;
 
-    class IObject {
-       public:
-        virtual ~IObject() = default;
+class IObject
+{
+   public:
+    virtual ~IObject() = default;
 
-        class IObjectError : public std::exception {};
+    class IObjectError : public std::exception
+    {};
 
-        virtual std::unique_ptr<IObject> clone() const = 0;
+    virtual std::unique_ptr<IObject> clone() const = 0;
 
-        virtual void setPosition(raytracer::Vector3<int> pos) = 0;
-        [[nodiscard]] virtual raytracer::Vector3<int> getPosition() const = 0;
+    virtual void setPosition(raytracer::Vector3<int> pos) = 0;
+    [[nodiscard]] virtual raytracer::Vector3<int> getPosition() const = 0;
 
-        virtual void setColor(raytracer::Vector3<int> rgb) = 0;
-        virtual raytracer::Vector3<int> getColor() const = 0;
+    virtual void setColor(raytracer::Vector3<double> rgb) = 0;
+    virtual void setColor(double r, double g, double b) = 0;
+    virtual raytracer::Vector3<double> getColor() const = 0;
+    virtual void setTransparence(double value) = 0;
+    virtual double getTransparence() const = 0;
+    virtual void setReflection(double value) = 0;
+    virtual double getReflection() const = 0;
 
-        virtual std::string getType() const = 0;
+    virtual std::string getType() const = 0;
 
-        virtual bool hit(const raytracer::Raycast &ray) const = 0;
+    virtual bool hit(const raytracer::Raycast &ray) const = 0;
 
-        virtual metaData_t &getMetaData() = 0;
-    };
+    virtual metaData_t &getMetaData() = 0;
+};
 
-    extern "C" std::unique_ptr<objects::IObject> createPrimitive(void);
+extern "C" std::unique_ptr<objects::IObject> createPrimitive(void);
 }  // namespace objects

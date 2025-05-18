ce1d493845f22085ae015727d4fe22269dc360dc
Pomalone <o.c.loulou@gmail.com>
2025-05-18 14:14:14 +0200

    feat: add doc for parser interface and abstract class

--- Parsers/AParser.hpp
+++ Parsers/AParser.hpp
@@ -14,17 +14,36 @@
 #include <string>
 
 namespace raytracer {
+
+    ///
+    /// \class AParser
+    /// \brief Abstract base class providing parsing functionality for raytracer objects.
+    ///
     class AParser : public IParser {
        public:
+        ///
+        /// \brief Destructor.
+        ///
         ~AParser() override = default;
 
+        ///
+        /// \brief Sets the filename to be parsed.
+        /// \param filename The name of the file to parse.
+        ///
         void setFilename(const std::string &filename) override
         {
             this->_filename = filename;
         };
 
-        void retrieveObjects() override = 0;
+        ///
+        /// \brief Retrieves and parses objects from the file.
+        ///
+        virtual void retrieveObjects() override = 0;
 
+        ///
+        /// \brief Sets the list of available objects for parsing.
+        /// \param availableObjects A vector containing unique pointers to available objects.
+        ///
         void _setAvailableObject(
             std::vector<std::unique_ptr<objects::IObject>> availableObjects)
             override
@@ -32,11 +51,19 @@ namespace raytracer {
             this->_availableObjects = std::move(availableObjects);
         };
 
+        ///
+        /// \brief Retrieves the list of primitive objects.
+        /// \return A vector of unique pointers to primitive objects.
+        ///
         std::vector<std::unique_ptr<BasicObject>> getPrimitives() override
         {
             return std::move(this->_Primitives);
         };
 
+        ///
+        /// \brief Retrieves the camera object.
+        /// \return A unique pointer to the camera object.
+        ///
         std::unique_ptr<BasicObject> getCamera() override
         {
             std::unique_ptr NewCam = std::move(this->_Camera);
@@ -44,6 +71,10 @@ namespace raytracer {
             return NewCam;
         };
 
+        ///
+        /// \brief Retrieves the light objects.
+        /// \return A unique pointer to the light objects.
+        ///
         std::unique_ptr<BasicObject> getLights() override
         {
             std::unique_ptr newLights = std::move(this->_Lights);
@@ -52,10 +83,11 @@ namespace raytracer {
         }
 
        protected:
-        std::string _filename = "empty";
-        std::vector<std::unique_ptr<raytracer::BasicObject>> _Primitives;
-        std::vector<std::unique_ptr<objects::IObject>> _availableObjects;
-        std::unique_ptr<BasicObject> _Camera;
-        std::unique_ptr<BasicObject> _Lights;
+        std::string _filename = "empty"; ///< The filename to be parsed.
+        std::vector<std::unique_ptr<raytracer::BasicObject>> _Primitives; ///< List of primitive objects.
+        std::vector<std::unique_ptr<objects::IObject>> _availableObjects; ///< List of available objects for parsing.
+        std::unique_ptr<BasicObject> _Camera; ///< Unique pointer to camera object.
+        std::unique_ptr<BasicObject> _Lights; ///< Unique pointer to light objects.
     };
+
 }  // namespace raytracer

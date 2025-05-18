8ea9fcb5f6b424f8a5dd53de5157caba8fea4ed7
Valiance <lucas.ludovic@epitech.eu>
2025-05-18 20:45:10 +0200

    doc: add doc for modules

--- Raytracer/src/Modules/Camera/src/Camera.hpp
+++ Raytracer/src/Modules/Camera/src/Camera.hpp
@@ -10,26 +10,74 @@
 #include "Vector.hpp"
 
 namespace objects {
+
+    /**
+     * @class Camera
+     * @brief Represents a camera in a 3D scene.
+     *
+     * The Camera class defines the viewpoint in the ray tracing environment.
+     * It holds the position, rotation, resolution, and field of view settings.
+     */
     class Camera {
        public:
+        /**
+         * @brief Default constructor.
+         */
         Camera();
 
+        /**
+         * @brief Sets the camera position in 3D space.
+         * @param pos A 3D vector representing the position.
+         */
         void setPosition(raytracer::Vector3<int> pos);
+
+        /**
+         * @brief Gets the camera position.
+         * @return A 3D vector representing the camera position.
+         */
         raytracer::Vector3<int> getPosition() const;
 
+        /**
+         * @brief Sets the resolution of the camera.
+         * @param res A 2D vector representing the resolution (width x height).
+         */
         void setResolution(raytracer::Vector2<int> res);
+
+        /**
+         * @brief Gets the camera resolution.
+         * @return A 2D vector representing the resolution.
+         */
         raytracer::Vector2<int> getResolution() const;
 
+        /**
+         * @brief Sets the camera rotation in 3D space.
+         * @param rotation A 3D vector representing the rotation angles (pitch, yaw, roll).
+         */
         void setRotation(raytracer::Vector3<int> rotation);
+
+        /**
+         * @brief Gets the camera rotation.
+         * @return A 3D vector representing the camera's rotation.
+         */
         raytracer::Vector3<int> getRotation() const;
 
+        /**
+         * @brief Sets the camera's field of view.
+         * @param value Field of view in degrees.
+         */
         void setFieldOfView(int value);
+
+        /**
+         * @brief Gets the field of view of the camera.
+         * @return The field of view in degrees.
+         */
         int getFieldOfView() const;
 
        private:
-        raytracer::Vector2<int> _res;
-        raytracer::Vector3<int> _rotation;
-        raytracer::Vector3<int> _position;
-        int _fieldOfView = 0;
+        raytracer::Vector2<int> _res;        ///< Resolution of the camera (width x height).
+        raytracer::Vector3<int> _rotation;   ///< Rotation of the camera (pitch, yaw, roll).
+        raytracer::Vector3<int> _position;   ///< Position of the camera in 3D space.
+        int _fieldOfView = 0;                ///< Field of view in degrees.
     };
+
 }  // namespace objects
--- Raytracer/src/Modules/Lights/src/Lights.hpp
+++ Raytracer/src/Modules/Lights/src/Lights.hpp
@@ -11,28 +11,64 @@
 #include <vector>
 
 namespace objects {
+
+    /**
+     * @class Lights
+     * @brief Represents the lighting configuration in a 3D scene.
+     *
+     * The Lights class defines ambient and diffuse light components,
+     * as well as a list of point light sources in 3D space.
+     */
     class Lights {
        public:
+        /**
+         * @brief Default constructor.
+         */
         Lights();
 
+        /**
+         * @brief Gets the ambient light intensity.
+         * @return A double representing the ambient light level.
+         */
         double getAmbient() const { return this->_ambient; }
 
+        /**
+         * @brief Gets the diffuse light intensity.
+         * @return A double representing the diffuse light level.
+         */
         double getDiffuse() const { return this->_diffuse; }
 
+        /**
+         * @brief Gets the list of point light sources.
+         * @return A vector of 3D vectors representing point light positions.
+         */
         std::vector<raytracer::Vector3<int>> getPoint() const { return this->_point; }
 
+        /**
+         * @brief Sets the ambient light intensity.
+         * @param ambient New ambient light level.
+         */
         void setAmbient(double ambient) { this->_ambient = ambient; }
 
+        /**
+         * @brief Sets the diffuse light intensity.
+         * @param diffuse New diffuse light level.
+         */
         void setDiffuse(double diffuse) { this->_diffuse = diffuse; }
 
+        /**
+         * @brief Sets the point light sources.
+         * @param newPoint A vector of 3D vectors representing new point light positions.
+         */
         void setPoint(const std::vector<raytracer::Vector3<int>> &newPoint)
         {
             this->_point = newPoint;
         }
 
        private:
-        double _ambient = 0;
-        double _diffuse = 0;
-        std::vector<raytracer::Vector3<int>> _point;
+        double _ambient = 0; ///< Ambient light intensity.
+        double _diffuse = 0; ///< Diffuse light intensity.
+        std::vector<raytracer::Vector3<int>> _point; ///< Positions of point light sources.
     };
+
 }  // namespace objects
